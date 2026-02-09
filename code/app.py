27c16903d0da7d66936132c586909eaee66cce75
Lukas Brunner <lukas.brunner@live.at>
2026-02-09 17:12:54 +0100

    update

--- code/app.py
+++ code/app.py
@@ -261,7 +261,7 @@ def download_plot():
         dpi=150,
     )
     buf = io.BytesIO()
-    fig.savefig(buf, format='png', bbox_inches='tight')
+    fig.savefig(buf, format='png', dpi=150, bbox_inches='tight')
     plt.close(fig)
     buf.seek(0)
     yield buf.getvalue()
